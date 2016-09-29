994b906cb2f7fcbacb37ae27961a7944c95246f5
preethijayanthi <preethi8895@gmail.com>
2016-09-29 15:35:56 +0530

    Add files via upload

---
+++ Dyanamic.java
@@ -0,0 +1,34 @@
+class Electronicdevices
+{
+	void operation()
+	{
+	System.out.println("The devices can used to hear,see films,etc...");
+	}
+}
+class Radio extends Electronicdevices
+{	
+	void operation()
+	{
+	System.out.println("This device can used to hear songs");
+	}
+}
+class TV extends Electronicdevices
+{
+	void operation()
+	{
+	System.out.println("This device can used to see film");
+	}
+}
+class Dyanamic
+{
+public static void main(String ar[])
+{
+Electronicdevices e;
+e=new Radio();
+e.operation();
+e=new TV();
+e.operation();
+}
+}
+
+		
